291046c2b41027cdbc2e24747564216aa3ca3711
nitika vaish <vaishnitika95@gmail.com>
2026-01-05 14:39:21 +0530

    modified

--- src/Stacks/InterviewQuestions/balenceBrackets.java
+++ src/Stacks/InterviewQuestions/balenceBrackets.java
@@ -1,8 +1,32 @@
 package Stacks.InterviewQuestions;
 
+import java.util.*;
+
 public class balenceBrackets {
     // check whether a given bracket is balanced or not
-    public static void main(String[] args) {
+    public static boolean checkBalence(String s) {
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                st.push('(');
+            } else {
+                if (st.isEmpty()) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        if (st.isEmpty()) {
+            return true;
+        }
+        return false;
 
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        boolean check = checkBalence(s);
+        System.out.println(check);
     }
 }
