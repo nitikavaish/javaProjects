dd6e14ce60d0a4cd2caf107f30ac789249642ec2
nitika vaish <vaishnitika95@gmail.com>
2026-01-23 21:13:09 +0530

    added new files

--- src/Queue/InterviewQuestions/reverseQueue.java
+++ src/Queue/InterviewQuestions/reverseQueue.java
@@ -1,4 +1,27 @@
 package Queue.InterviewQuestions;
 
+import java.util.*;
+
 public class reverseQueue {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        Stack<Integer> st = new Stack<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        System.out.println(q);
+        while (!q.isEmpty()) {
+            st.push(q.remove());
+        }
+        while (!st.isEmpty()) {
+            q.add(st.pop());
+        }
+        System.out.println("after reverse");
+        System.out.println(q);
+
+
+    }
 }
--- src/Queue/InterviewQuestions/reverse_k_Elements.java
+++ src/Queue/InterviewQuestions/reverse_k_Elements.java
@@ -1,4 +1,27 @@
 package Queue.InterviewQuestions;
 
+import java.util.*;
+
 public class reverse_k_Elements {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        Stack<Integer> st = new Stack<>();
+        int k = 4;
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        for (int i = 1; i <= k; i++) {
+            st.push(q.remove());
+        }
+        while (!st.isEmpty()) {
+            q.add(st.pop());
+        }
+        for (int i = 1; i <= q.size() - k; i++) {
+            int top = q.remove();
+            q.add(top);
+        }
+        System.out.println(q);
+    }
 }
